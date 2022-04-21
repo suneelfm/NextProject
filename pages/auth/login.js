5faacdfab2f2a18f175ecd34339bf4e1b3c293e1
suneelfm <sfm.20031998@gmail.com>
2022-04-21 22:41:09 +0530

    Added login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,6 +1,35 @@
+import { Button, Grid } from "@mui/material";
+import styles from "../../styles/Login.module.css";
 
 export default function Login() {
   return (
-    <div>login</div>
-  )
+    <Grid container className={styles.loginBody}>
+      <form className={styles.loginForm}>
+        <Grid container justifyContent={"center"}>
+          <h1>Login</h1>
+        </Grid>
+        <Grid
+          container
+          justifyContent={"center"}
+          className={styles.filedContainer}
+        >
+          <input type={"text"} className={styles.inputField} />
+        </Grid>
+        <Grid
+          container
+          justifyContent={"center"}
+          className={styles.filedContainer}
+        >
+          <input type={"password"} className={styles.inputField} />
+        </Grid>
+        <Grid
+          container
+          justifyContent={"center"}
+          className={styles.filedContainer}
+        >
+          <Button className={styles.inputField} variant="contained">Login</Button>
+        </Grid>
+      </form>
+    </Grid>
+  );
 }
