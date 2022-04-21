c317f33be22b9a3af8461429e49941a9ce4e644d
suneelfm <sfm.20031998@gmail.com>
2022-04-22 00:53:26 +0530

    Validated layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,18 +1,33 @@
 import { Grid } from "@mui/material";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import styles from "../../styles/Layout.module.css";
 
 export default function Layout({ Component, props }) {
-  return (
-    <Grid container className={styles.mainBody}>
-      <Grid container className={styles.headerContainer}>
-        dfg
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (status !== "authenticated") {
+      router.push("/auth/login");
+    }
+  }, [session]);
+
+  if (status !== "authenticated") {
+    return <Component {...props} />;
+  } else {
+    return (
+      <Grid container className={styles.mainBody}>
+        <Grid container className={styles.headerContainer}>
+          dfg
+        </Grid>
+        <Grid container className={styles.sidebarContainer}>
+          dfg
+        </Grid>
+        <Grid item className={styles.routingOutlet}>
+          <Component {...props} />
+        </Grid>
       </Grid>
-      <Grid container className={styles.sidebarContainer}>
-        dfg
-      </Grid>
-      <Grid item className={styles.routingOutlet}>
-        <Component {...props} />
-      </Grid>
-    </Grid>
-  );
+    );
+  }
 }
