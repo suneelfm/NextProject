54d60c97e5798032d86301bcee2eb6fb8154f268
suneelfm <sfm.20031998@gmail.com>
2022-04-23 00:08:05 +0530

    Updated productdisplay

--- components/home/productDisplay.js
+++ components/home/productDisplay.js
@@ -1,4 +1,4 @@
-import { Card, CardMedia, Grid, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 import styles from "../../styles/Components.module.css";
 
@@ -6,6 +6,8 @@ export default function ProductDisplay({
   products,
   listTital,
   discount = "Upto 30% Off",
+  bottomLabelReqired,
+  cardFor,
 }) {
   return (
     <>
@@ -13,8 +15,15 @@ export default function ProductDisplay({
         {listTital}
       </Grid>
       <Grid container spacing={3}>
-        {products.map((image, index) => (
-          <Grid key={index} item xs={12} md={6} lg={3} height="250px">
+        {products?.map((image, index) => (
+          <Grid
+            key={index}
+            item
+            xs={12}
+            md={6}
+            lg={3}
+            height={cardFor === "movies" ? "300px" : "250px"}
+          >
             {/* <Link to={"/nykaa/products"}> */}
             <Card
               className={styles.productCard}
@@ -22,26 +31,31 @@ export default function ProductDisplay({
             >
               <CardMedia
                 component="img"
-                height="100%"
-                image={image}
+                height={cardFor === "movies" ? "75%" : "100%"}
+                image={cardFor === "movies" ? image.Poster : image}
                 alt="Paella dish"
               ></CardMedia>
-              <Grid
-                zIndex={2}
-                position="absolute"
-                bottom={0}
-                left="0.45vw"
-                height="3vw"
-                width={"96%"}
-                className={styles.productCardBottomContent}
-              >
-                <Typography sx={{ fontSize: "1vw", color: "red" }}>
-                  {discount}
-                </Typography>
-                <Typography sx={{ fontSize: "0.8vw" }}>
-                  On International Bestsellers!
-                </Typography>
-              </Grid>
+              {bottomLabelReqired && (
+                <Grid
+                  zIndex={2}
+                  position="absolute"
+                  bottom={0}
+                  left="0.45vw"
+                  height="3vw"
+                  width={"96%"}
+                  className={styles.productCardBottomContent}
+                >
+                  <Typography sx={{ fontSize: "1vw", color: "red" }}>
+                    {discount}
+                  </Typography>
+                  <Typography sx={{ fontSize: "0.8vw" }}>
+                    On International Bestsellers!
+                  </Typography>
+                </Grid>
+              )}
+              <CardContent>
+                <b>{image.Title}</b>
+              </CardContent>
             </Card>
             {/* </Link> */}
           </Grid>
