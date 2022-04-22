4b3e82fde2446426b46daca98e9d0c5a9b95941b
suneelfm <sfm.20031998@gmail.com>
2022-04-23 00:07:07 +0530

    Updated Home

--- components/home/home.js
+++ components/home/home.js
@@ -14,13 +14,28 @@ export default function HomePage() {
     "../images8.jpg",
   ];
 
+  const movies = [
+    { Poster: "../movies/download1.jfif", Title: "2020 B2B" },
+    { Poster: "../movies/download2.jfif", Title: "KGF" },
+    { Poster: "../movies/download3.jfif", Title: "RRR" },
+    { Poster: "../movies/download4.jfif", Title: "Best Friend" },
+    { Poster: "../movies/download5.jfif", Title: "2020 B2B" },
+    { Poster: "../movies/download6.jfif", Title: "Ulidavaru Kandante" },
+    { Poster: "../movies/download7.jfif", Title: "Kalave Mosagar" },
+    { Poster: "../movies/download8.jfif", Title: "Ayushmanbhava" },
+  ];
+
   return (
     <>
       <Grid container item height={"25vw"} position={"relative"} top="0.3vw">
         <OfferSlideShow></OfferSlideShow>
       </Grid>
-      <ProductDisplay products={images} listTital="TOP BRANDS" />
-      <ProductDisplay products={images} listTital="ONLY AT NYKAA" />
+      <ProductDisplay products={movies} listTital="Movies" cardFor={"movies"} />
+      <ProductDisplay
+        products={images}
+        listTital="TOP BRANDS"
+        bottomLabelReqired={true}
+      />
     </>
   );
 }
