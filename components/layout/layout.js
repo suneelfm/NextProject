22be7d3ac0f3ee4fdff78b782090278aada9dad7
suneelfm <sfm.20031998@gmail.com>
2022-04-23 23:28:03 +0530

    Fixed data fetch on load

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -14,7 +14,7 @@ import { useEffect, useState } from "react";
 import Login from "../../pages/auth/login";
 import styles from "../../styles/Layout.module.css";
 
-export default function Layout({ Component, props }) {
+export default function Layout({ Component, pageProps }) {
   const [openMenu, setopenMenu] = useState(false);
   const [openProfileOptions, setopenProfileOptions] = useState(false);
   const { data: session, status } = useSession();
@@ -30,107 +30,105 @@ export default function Layout({ Component, props }) {
   } else if (status === "authenticated") {
     return (
       <Grid container className={styles.mainBody}>
-        {status === "authenticated" && (
-          <>
-            <Grid container xs={12} className={styles.headerContainer}>
-              <Grid
-                container
-                item
-                xs={1}
-                className={styles.menuIcon}
-                alignItems="center"
-              >
-                <FontAwesomeIcon
-                  icon={faBars}
-                  onClick={() => setopenMenu(!openMenu)}
-                />
-              </Grid>
-              <Grid item xs={5} ml="4vw"></Grid>
-              <Grid container item xs={3} justifyContent={"right"}>
-                <Grid className={styles.searchField}>
-                  <Grid>
-                    <div className={styles.searchIcon}>
-                      <FontAwesomeIcon icon={faSearch} />
-                    </div>
-                    <input
-                      type={"search"}
-                      className={styles.searchInput}
-                      placeholder="Search"
-                    />
-                  </Grid>
+        <>
+          <Grid container xs={12} className={styles.headerContainer}>
+            <Grid
+              container
+              item
+              xs={1}
+              className={styles.menuIcon}
+              alignItems="center"
+            >
+              <FontAwesomeIcon
+                icon={faBars}
+                onClick={() => setopenMenu(!openMenu)}
+              />
+            </Grid>
+            <Grid item xs={5} ml="4vw"></Grid>
+            <Grid container item xs={3} justifyContent={"right"}>
+              <Grid className={styles.searchField}>
+                <Grid>
+                  <div className={styles.searchIcon}>
+                    <FontAwesomeIcon icon={faSearch} />
+                  </div>
+                  <input
+                    type={"search"}
+                    className={styles.searchInput}
+                    placeholder="Search"
+                  />
                 </Grid>
-              </Grid>
-              <Grid
-                container
-                position={"relative"}
-                item
-                xs={1}
-                justifyContent={"right"}
-              >
-                <FontAwesomeIcon
-                  icon={faShoppingBag}
-                  // onClick={() => setshowBag(true)}
-                  className={styles.cartIcon}
-                />
-              </Grid>
-              <Grid
-                container
-                item
-                xs={1}
-                justifyContent={"right"}
-                alignItems="center"
-              >
-                <label
-                  htmlFor="account"
-                  onClick={() => setopenProfileOptions(!openProfileOptions)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <FontAwesomeIcon id="account" icon={faUser} />
-                  <span style={{ marginLeft: "0.3vw", fontSize: "1vw" }}>
-                    Account
-                  </span>
-                </label>
               </Grid>
             </Grid>
             <Grid
               container
-              className={styles.sidebarContainer}
-              width={openMenu ? "10%" : "3%"}
+              position={"relative"}
+              item
+              xs={1}
+              justifyContent={"right"}
             >
-              {openMenu && (
-                <Grid item xs={12}>
-                  <MenuList>
-                    <Link href={"/home"} passHref>
-                      <MenuItem className={styles.menuItems}>
-                        <b>Home</b>
-                      </MenuItem>
-                    </Link>
-                    <Link href={"/todo"} passHref>
-                      <MenuItem className={styles.menuItems}>
-                        <b>Todo</b>
-                      </MenuItem>
-                    </Link>
-                    <Link href={"/shopping"} passHref>
-                      <MenuItem className={styles.menuItems}>
-                        <b>Shopping</b>
-                      </MenuItem>
-                    </Link>
-                    <Link href={"/movies"} passHref>
-                      <MenuItem className={styles.menuItems}>
-                        <b>Movies</b>
-                      </MenuItem>
-                    </Link>
-                    <MenuItem className={styles.menuItems}>
-                      <a href="https://www.nykaa.com/nykaa-network/home">
-                        <b className="nykaatabs">Nykaa Network</b>
-                      </a>
-                    </MenuItem>
-                  </MenuList>
-                </Grid>
-              )}
+              <FontAwesomeIcon
+                icon={faShoppingBag}
+                // onClick={() => setshowBag(true)}
+                className={styles.cartIcon}
+              />
             </Grid>
-          </>
-        )}
+            <Grid
+              container
+              item
+              xs={1}
+              justifyContent={"right"}
+              alignItems="center"
+            >
+              <label
+                htmlFor="account"
+                onClick={() => setopenProfileOptions(!openProfileOptions)}
+                style={{ cursor: "pointer" }}
+              >
+                <FontAwesomeIcon id="account" icon={faUser} />
+                <span style={{ marginLeft: "0.3vw", fontSize: "1vw" }}>
+                  Account
+                </span>
+              </label>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            className={styles.sidebarContainer}
+            width={openMenu ? "10%" : "3%"}
+          >
+            {openMenu && (
+              <Grid item xs={12}>
+                <MenuList>
+                  <Link href={"/home"} passHref>
+                    <MenuItem className={styles.menuItems}>
+                      <b>Home</b>
+                    </MenuItem>
+                  </Link>
+                  <Link href={"/todo"} passHref>
+                    <MenuItem className={styles.menuItems}>
+                      <b>Todo</b>
+                    </MenuItem>
+                  </Link>
+                  <Link href={"/shopping"} passHref>
+                    <MenuItem className={styles.menuItems}>
+                      <b>Shopping</b>
+                    </MenuItem>
+                  </Link>
+                  <Link href={"/movies"} passHref>
+                    <MenuItem className={styles.menuItems}>
+                      <b>Movies</b>
+                    </MenuItem>
+                  </Link>
+                  <MenuItem className={styles.menuItems}>
+                    <a href="https://www.nykaa.com/nykaa-network/home">
+                      <b className="nykaatabs">Nykaa Network</b>
+                    </a>
+                  </MenuItem>
+                </MenuList>
+              </Grid>
+            )}
+          </Grid>
+        </>
         {openProfileOptions && (
           <Grid item className={styles.profileOptions}>
             <MenuList className={styles.profileOptionsList}>
@@ -150,7 +148,7 @@ export default function Layout({ Component, props }) {
           className={styles.routingOutlet}
           width={openMenu ? "89%" : "96%"}
         >
-          <Component {...props} />
+          <Component {...pageProps} />
         </Grid>
       </Grid>
     );
--- pages/todo/index.js
+++ pages/todo/index.js
@@ -3,20 +3,8 @@ import React, { useEffect } from "react";
 import TodoPage from "../../components/todo/todoPage";
 
 export default function Todo(props) {
-  useEffect(() => {
-    console.log(props);
-  }, []);
-
   return <TodoPage data={props} />;
 }
-
-// export async function getStaticProps() {
-//   const res = await axios.get(`https://asmita-mern.herokuapp.com/todo/get`);
-//   const data = res.data;
-//   return {
-//     props: { data }, // will be passed to the page component as props
-//   };
-// }
 
 export async function getServerSideProps() {
   const res = await axios.get(`https://asmita-mern.herokuapp.com/todo/get`);
